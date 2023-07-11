a39a6c16de9439d84b9def7127f0b10eb7db5772
Marie Tornetto <69056770+MarieT88@users.noreply.github.com>
2023-07-10 23:08:01 -0400

    clean up code

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,12 +1,5 @@
 const Sequelize = require('sequelize');
 
-//const conn = new Sequelize(process.env.DATABASE_URL || 'mysql://marie:password:@localhost:3306/split-the-bill');
-
-/*const conn = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  // Other configuration options
-});*/
-
 const conn = new Sequelize(process.env.JAWSDB_URL || 'mysql://marie:password@localhost:3306/split-the-bill?authPlugins=mysql_native_password', {
   dialect: 'mysql',
 });
